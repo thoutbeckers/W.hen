6b37f394067031e24a7fb0cd5db3529fdfd9634d
Jasper van Putten <jasper@splendo.com>
2014-03-25 22:55:13 +0100

    add color to menuItem icon

---
+++ WhenRobo/src/main/java/houtbecke/rs/when/robo/act/ColorMenuItem.java
@@ -0,0 +1,24 @@
+package houtbecke.rs.when.robo.act;
+
+import android.graphics.PorterDuff;
+import android.view.MenuItem;
+
+import com.squareup.otto.Bus;
+
+public class ColorMenuItem extends BaseUpdateMenuItem {
+
+    @javax.inject.Inject
+    public ColorMenuItem(Bus bus) {
+        super(bus);
+    }
+
+    @Override
+    public void changeMenu(MenuItem menuItem, int color) {
+        menuItem.getIcon().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+    }
+    @Override
+    public void changeMenu(MenuItem menuItem) {
+        menuItem.getIcon().clearColorFilter();
+    }
+
+}
