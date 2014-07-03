104296eb9051f3c83b68a50539af659cf0c206cc
Tijl Houtbeckers <tijl@splendo.nl>
2014-07-03 16:51:54 +0200

    NotActive condition

--- WhenRobo/src/main/java/houtbecke/rs/when/robo/condition/Active.java
+++ WhenRobo/src/main/java/houtbecke/rs/when/robo/condition/Active.java
@@ -1,61 +1,15 @@
 package houtbecke.rs.when.robo.condition;
 
 import com.squareup.otto.Bus;
-import com.squareup.otto.Subscribe;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 
-import houtbecke.rs.when.BasePushCondition;
-import houtbecke.rs.when.robo.condition.event.ActivityPause;
-import houtbecke.rs.when.robo.condition.event.ActivityResume;
-import houtbecke.rs.when.robo.condition.event.FragmentPause;
-import houtbecke.rs.when.robo.condition.event.FragmentResume;
-import houtbecke.rs.when.robo.event.PauseEvent;
-import houtbecke.rs.when.robo.event.ResumeEvent;
-
-public class Active extends BasePushCondition {
-
-    // maybe use a weakhashmap to track behaviour for multiple object of same classes etc.
-
-    Bus bus;
+@Singleton
+public class Active extends BaseActive {
 
     @Inject
     public Active(Bus bus) {
-        bus.register(this);
-        this.bus = bus;
+        super(bus, true, false);
     }
-
-    @Subscribe
-    public void onActivityEvent(ActivityResume event) {
-       onResume(event);
-    }
-
-    @Subscribe
-    public void onActivityEvent(ActivityPause event) {
-        onPause(event);
-    }
-
-    @Subscribe
-    public void onFragmentEvent(FragmentResume event) {
-        onResume(event);
-    }
-
-    @Subscribe
-    public void onFragmentEvent(FragmentPause event) {
-        onPause(event);
-    }
-
-    void onPause(PauseEvent pause) {
-        stickForThing(pause.getSourceClass(), false);
-        stickForThing(pause.getObject(), false);
-        stickForThing(pause.getResourceId(), false);
-    }
-
-    void onResume(ResumeEvent resume) {
-        stickForThing(resume.getSourceClass(),true);
-        stickForThing(resume.getObject(),true);
-        stickForThing(resume.getResourceId(),true);
-    }
-
-
 }
---
+++ WhenRobo/src/main/java/houtbecke/rs/when/robo/condition/NotActive.java
@@ -0,0 +1,26 @@
+package houtbecke.rs.when.robo.condition;
+
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import houtbecke.rs.when.BasePushCondition;
+import houtbecke.rs.when.TypedPushCondition;
+import houtbecke.rs.when.robo.condition.event.ActivityPause;
+import houtbecke.rs.when.robo.condition.event.ActivityResume;
+import houtbecke.rs.when.robo.condition.event.FragmentPause;
+import houtbecke.rs.when.robo.condition.event.FragmentResume;
+import houtbecke.rs.when.robo.event.PauseEvent;
+import houtbecke.rs.when.robo.event.ResumeEvent;
+
+@Singleton
+public class NotActive extends BaseActive {
+
+    @Inject
+    public NotActive(Bus bus) {
+        super(bus, false, true);
+    }
+
+}
