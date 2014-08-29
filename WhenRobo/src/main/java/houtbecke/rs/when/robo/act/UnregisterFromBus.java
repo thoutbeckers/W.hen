ff2670ede84c6ea4c7f181e27aa5c6d63cb725d7
Tijl Houtbeckers <tijl@splendo.nl>
2014-08-29 19:14:54 +0200

    Condition with a Bus, and more UI actions for WhenRobo

---
+++ WhenRobo/src/main/java/houtbecke/rs/when/robo/act/UnregisterFromBus.java
@@ -0,0 +1,18 @@
+package houtbecke.rs.when.robo.act;
+
+import javax.inject.Singleton;
+
+import houtbecke.rs.when.robo.BusPushCondition;
+import houtbecke.rs.when.robo.OnUiThread;
+
+@Singleton
+public class UnregisterFromBus extends AndroidTypedAct {
+
+    @OnUiThread
+    public void act(BusPushCondition... busPushConditions) {
+        for (BusPushCondition busPushCondition : busPushConditions) {
+            busPushCondition.unregister();
+        }
+
+    }
+}
