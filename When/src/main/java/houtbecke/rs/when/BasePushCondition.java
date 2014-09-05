10bb03d5347718bcc7b77ebe677d5eee98f90248
Tijl Houtbeckers <tijl@splendo.nl>
2014-09-05 12:29:18 +0200

    synchronize the adding and removing of listeners of base push condition

--- When/src/main/java/houtbecke/rs/when/BasePushCondition.java
+++ When/src/main/java/houtbecke/rs/when/BasePushCondition.java
@@ -5,44 +5,54 @@ import java.util.LinkedHashMap;
 
 public class BasePushCondition implements PushCondition {
 
-    LinkedHashMap<Object, IdentityHashMap<PushConditionListener, Void>> listeners = new LinkedHashMap<Object, IdentityHashMap<PushConditionListener, Void>>(1);
+    final LinkedHashMap<Object, IdentityHashMap<PushConditionListener, Void>> listeners = new LinkedHashMap<Object, IdentityHashMap<PushConditionListener, Void>>(1);
 
     @Override
     public void removeListener(PushConditionListener listener, Object thing) {
-        IdentityHashMap<PushConditionListener, Void> pcls = listeners.get(thing);
-        if (pcls == null)
-            return;
-        if (pcls.size() <= 1)
-            listeners.remove(thing);
-        pcls.remove(listener);
+        synchronized (listeners) {
+
+            IdentityHashMap<PushConditionListener, Void> pcls = listeners.get(thing);
+            if (pcls == null)
+                return;
+            if (pcls.size() <= 1)
+                listeners.remove(thing);
+            pcls.remove(listener);
+        }
     }
 
     @Override
     public void addListener(PushConditionListener listener, Object thing) {
-        IdentityHashMap<PushConditionListener, Void> pcls = listeners.get(thing);
-        if (pcls != null && !pcls.keySet().contains(listener)) {
-            pcls.put(listener, null);
-        }
-        else {
-            pcls = new IdentityHashMap<PushConditionListener, Void>(1);
-            pcls.put(listener, null);
-            listeners.put(thing, pcls);
+
+        synchronized (listeners) {
+            IdentityHashMap<PushConditionListener, Void> pcls = listeners.get(thing);
+            if (pcls != null && !pcls.keySet().contains(listener)) {
+                pcls.put(listener, null);
+            } else {
+                pcls = new IdentityHashMap<PushConditionListener, Void>(1);
+                pcls.put(listener, null);
+                listeners.put(thing, pcls);
+            }
         }
     }
+
     public void push(Object thing, boolean isConditionMet, boolean isSticky, Object... results ) {
 
-        if (thing != null) {
-            IdentityHashMap<PushConditionListener, Void> pcls = listeners.get(thing);
-            if (pcls == null)
-                return;
-            for (PushConditionListener pcl: pcls.keySet())
-                pcl.push(this, isConditionMet, isSticky, results);
-            return;
-        }
+        synchronized(listeners) {
 
-        for (IdentityHashMap<PushConditionListener, Void> pcls: listeners.values()) {
-            for (PushConditionListener pcl: pcls.keySet())
-                pcl.push(this, isConditionMet, isSticky, results);
+            if (thing != null) {
+                IdentityHashMap<PushConditionListener, Void> pcls = listeners.get(thing);
+                if (pcls == null)
+                    return;
+                for (PushConditionListener pcl : pcls.keySet())
+                    pcl.push(this, isConditionMet, isSticky, results);
+                return;
+            }
+
+
+            for (IdentityHashMap<PushConditionListener, Void> pcls : listeners.values()) {
+                for (PushConditionListener pcl : pcls.keySet())
+                    pcl.push(this, isConditionMet, isSticky, results);
+            }
         }
     }
 
